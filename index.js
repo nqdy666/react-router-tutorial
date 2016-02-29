386b44b97a689b6e03b5cea6bd72c597e6967b9a
nianqin <bu__dong@163.com>
2016-02-29 17:09:21 +0800

    lesson06

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ import { render } from 'react-dom'
 import App from './modules/App'
 import About from './modules/About'
 import Repos from './modules/Repos'
+import Repo from './modules/Repo'
 
 render((
     <Router history={hashHistory}>
         <Route path="/" component={App}>
             <Route path="/repos" component={Repos} />
+            <Route path="/repos/:userName/:repoName" component={Repo}/>
             <Route path="/about" component={About} />
         </Route>
     </Router>
